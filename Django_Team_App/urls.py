8803cec8bf11d5ba498ace44551d06b37d11b890
myrsaliev21 <myrsalievdastan211104@gmail.com>
2024-07-26 20:01:58 +0600

    Dastan

--- Django_Team_App/urls.py
+++ Django_Team_App/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register
+from .views import register, add_product
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'),name='login'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('', register),
+    path('add_product/', add_product)
 ]
--- Django_Team_App/views.py
+++ Django_Team_App/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegister
+from .forms import UserRegister, ProductModelForm
 
 
 def register(request):
@@ -10,4 +10,15 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegister()
+    return render(request, 'register.html', {'form': form})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductModelForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+    else:
+        form = ProductModelForm()
     return render(request, 'register.html', {'form': form})
